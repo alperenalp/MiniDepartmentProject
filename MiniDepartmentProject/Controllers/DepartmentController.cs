b6d31661399452a64213bb59666a4dcf945eceb3
alperenalp <alperen.alp045@gmail.com>
2023-05-28 02:11:40 +0300

    Added create new department method

--- MiniDepartmentProject/Controllers/DepartmentController.cs
+++ MiniDepartmentProject/Controllers/DepartmentController.cs
@@ -6,10 +6,25 @@ namespace MiniDepartmentProject.Controllers
     public class DepartmentController : Controller
     {
         private Context _context = new Context();
+
         public IActionResult Index()
         {
             var departments = _context.Departments.ToList();
             return View(departments);
+        }
+
+        [HttpGet]
+        public IActionResult CreateDepartment()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateDepartment(Department department)
+        {
+            _context.Departments.Add(department);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
